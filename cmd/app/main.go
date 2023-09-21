2ab63894b315a907477d889b155e5f37276dd165
Natalia Loskutova <loskutovanl@gmail.com>
2023-09-21 09:54:27 +0300

    fix

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,8 @@
 package main
 
-// docker-compose up --build
-
 import (
 	"database/sql"
 	"fmt"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
 	"net/http"
 	"os"
 	"study/config"
@@ -15,8 +11,6 @@ import (
 	"study/internal/usecase/repo"
 
 	"github.com/go-chi/chi/v5"
-
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
@@ -26,7 +20,6 @@ import (
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetLevel(log.InfoLevel)
-	// "../../.env"
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("No .env file found: %s", err)
 		os.Exit(1)
@@ -37,33 +30,17 @@ func main() {
 	// загрузка переменных, подключение и отложенное закрытие базы данных
 	conf := config.New()
 	var (
-		//host     = conf.Host
+		host     = conf.Host
 		port     = conf.Port
 		password = conf.Password
 		dbname   = conf.Dbname
 		user     = conf.User
 	)
-
-	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, "host.docker.internal", port, dbname))
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
 		log.Error("Unable to open database:", err)
 	}
-
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		log.Error("Unable to open driver postgres with instance", err)
-	}
-
-	// "file://../../migrations"
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://migrations",
-		"postgres", driver)
-	if err != nil {
-		log.Error("Unable to migrate database:", err)
-	}
-
-	m.Up()
-	defer m.Close()
 
 	defer func() {
 		err = db.Close()
@@ -72,16 +49,13 @@ func main() {
 		}
 	}()
 
-	fmt.Println("Hello!")
 	// Use case
 	userUseCase := usecase.New(
 		repo.NewPostgreSQLClassicRepository(db),
 	)
-
 	// создание роутера и регистрация хендлеров
 	mux := chi.NewRouter()
 	v1.NewUserRoutes(mux, userUseCase)
-
 	err = http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
 		log.Error("Unable to listen and serve:", err)
